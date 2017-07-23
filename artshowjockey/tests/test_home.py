a291b7960eff503a2bee068419043efb2d436a3e
binaryfox <binary@foxontheinter.net>
2017-07-23 15:10:44 -0700

    Add tests for loading /

---
+++ artshowjockey/tests/test_home.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.test import Client, TestCase
+
+class HomeTests(TestCase):
+
+    def test_unauthenticated(self):
+        c = Client()
+        response = c.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Log in")
+        self.assertContains(response, "Register")
+
+    def test_superuser(self):
+        password = 'test_password'
+        admin = User.objects.create_superuser(
+            username='admin',
+            email='admin@artshow',
+            password=password)
+        c = Client()
+        c.login(username=admin.username, password=password)
+        response = c.get(reverse('home'))
+        self.assertRedirects(response, reverse('artshow-home'))
+
+    def test_artist(self):
+        password = 'test_password'
+        artist = User.objects.create_user(
+            username='artist',
+            email='artist@artshow',
+            password=password)
+        c = Client()
+        c.login(username=artist.username, password=password)
+        response = c.get(reverse('home'))
+        self.assertRedirects(response, reverse('artshow-manage'))
+
