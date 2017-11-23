701c7619f7b3c1cd6cc951aca7019f2eaafcc999
binaryfox <binary@foxontheinter.net>
2017-11-23 11:23:27 -0800

    Resolve flake8 errors in artshow/bidderreg.py

--- artshow/bidderreg.py
+++ artshow/bidderreg.py
@@ -8,13 +8,13 @@ from formtools.wizard.views import CookieWizardView
 from .conf import settings
 from .models import Bidder, BidderId
 import logging
-logger = logging.getLogger(__name__)
 from .conf import _DISABLED as SETTING_DISABLED
 
 
 Person = apps.get_model(settings.ARTSHOW_PERSON_CLASS)
 
 
+logger = logging.getLogger(__name__)
 preprint = __import__(settings.ARTSHOW_PREPRINT_MODULE, globals(), locals(),
                       ['bidder_agreement'])
 
@@ -24,29 +24,43 @@ class BidderRegistrationForm0(forms.Form):
 
 
 class BidderRegistrationForm1(forms.Form):
-    name = forms.CharField(max_length=100,
-                           help_text="Your real, first and last name. This should match your identification.")
-    reg_id = forms.CharField(max_length=20,
-                             help_text="The number on the front of your convention badge. It looks like 123-456, and may have fewer or more digits. Enter the dash, too.")
-    cell_contact = forms.CharField(max_length=40, required=False,
-                                   help_text="If you have one, provide your cell number or that of a friend.")
-    other_contact = forms.CharField(max_length=100, required=False,
-                                    help_text="Optionally, another way to contact you during the convention, such as your hotel and room number.")
-    details_changed = forms.BooleanField(initial=False, required=False,
-                                         help_text="When you registered at the convention, you provided your address and e-mail. Have these changed since then?")
+    name = forms.CharField(
+        max_length=100,
+        help_text="Your real, first and last name. This should match your "
+                  "identification.")
+    reg_id = forms.CharField(
+        max_length=20,
+        help_text="The number on the front of your convention badge. It looks "
+                  "like 123-456, and may have fewer or more digits. Enter the "
+                  "dash, too.")
+    cell_contact = forms.CharField(
+        max_length=40, required=False,
+        help_text="If you have one, provide your cell number or that of a "
+                  "friend.")
+    other_contact = forms.CharField(
+        max_length=100, required=False,
+        help_text="Optionally, another way to contact you during the "
+                  "convention, such as your hotel and room number.")
+    details_changed = forms.BooleanField(
+        initial=False, required=False,
+        help_text="When you registered at the convention, you provided your "
+                  "address and e-mail. Have these changed since then?")
 
     def clean(self):
         cleaned_data = super(BidderRegistrationForm1, self).clean()
         cell_contact = cleaned_data.get("cell_contact")
         other_contact = cleaned_data.get("other_contact")
         if cell_contact == "" and other_contact == "":
-            raise forms.ValidationError("Please enter at least one way to contact you at the show.")
+            raise forms.ValidationError(
+                "Please enter at least one way to contact you at the show.")
         reg_id = cleaned_data.get("reg_id")
         if not settings.ARTSHOW_REGID_NONUNIQUE:
             matched_people = Person.objects.filter(reg_id=reg_id)
-            if BidderId.objects.filter(bidder__person__in=matched_people).exists():
+            if BidderId.objects.filter(
+                    bidder__person__in=matched_people).exists():
                 raise forms.ValidationError(
-                    "We think you have already been issued a Bidder ID. If this is unexpected, please see Staff immediately")
+                    "We think you have already been issued a Bidder ID. If "
+                    "this is unexpected, please see Staff immediately")
         return cleaned_data
 
 
@@ -68,9 +82,12 @@ def do_print_bidder_registration_form(bidder):
     preprint.bidder_agreement(bidder, sbuf)
 
     if settings.ARTSHOW_PRINT_COMMAND is SETTING_DISABLED:
-        logger.error("Cannot print agreement. ARTSHOW_PRINT_COMMAND is DISABLED")
+        logger.error("Cannot print agreement. ARTSHOW_PRINT_COMMAND is "
+                     "DISABLED")
         raise Exception("Printing is DISABLED in configuration")
-    p = subprocess.Popen(settings.ARTSHOW_PRINT_COMMAND, stderr=subprocess.PIPE, stdout=subprocess.PIPE,
+    p = subprocess.Popen(settings.ARTSHOW_PRINT_COMMAND,
+                         stderr=subprocess.PIPE,
+                         stdout=subprocess.PIPE,
                          stdin=subprocess.PIPE, shell=True)
     output, error = p.communicate(sbuf.getvalue())
     if output:
@@ -95,7 +112,8 @@ class BidderRegistrationWizard(CookieWizardView):
                 )
                 cell_contact = cleaned_data.get('cell_contact', '')
                 other_contact = cleaned_data.get('other_contact', '')
-                at_con_contact = "\n".join((x for x in [cell_contact, other_contact] if x))
+                at_con_contact = \
+                    "\n".join((x for x in [cell_contact, other_contact] if x))
                 b = Bidder(at_con_contact=at_con_contact)
             elif isinstance(form, BidderRegistrationForm2):
                 p.address1 = cleaned_data.get('address1', '')
@@ -107,15 +125,18 @@ class BidderRegistrationWizard(CookieWizardView):
                 p.phone = cleaned_data.get('phone', '')
                 p.email = cleaned_data.get('email', '')
         if b is None:
-            raise forms.ValidationError("End of wizard without creating bidder.")
+            raise forms.ValidationError(
+                    "End of wizard without creating bidder.")
         if p is None:
-            raise forms.ValidationError("End of wizard without creating person.")
+            raise forms.ValidationError(
+                    "End of wizard without creating person.")
         p.save()
         b.person = p
         b.save()
         # TODO: Print bidder registration form on the final page.
-        #do_print_bidder_registration_form(b)
+        # do_print_bidder_registration_form(b)
         return redirect(final)
+
 
 @permission_required('artshow.is_artshow_kiosk')
 def final(request):
@@ -125,6 +146,7 @@ def final(request):
 def process_step_2(wizard):
     cleaned_data = wizard.get_cleaned_data_for_step('1') or {}
     return cleaned_data.get('details_changed', False)
+
 
 wizard_view = permission_required('artshow.is_artshow_kiosk')(
     BidderRegistrationWizard.as_view([
