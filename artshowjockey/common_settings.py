956306ece59d7bcf60bdde932ddc303f0dcc289a
binaryfox <binary@foxontheinter.net>
2018-01-20 15:20:56 -0800

    Update to reCAPTCHA v2

--- artshowjockey/common_settings.py
+++ artshowjockey/common_settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'nocaptcha_recaptcha',
     'peeps',
     'artshow',
     'ajax_select',
