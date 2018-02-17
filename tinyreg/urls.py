4f90d8c95af9d04e2587e0542883b4e58ed6628a
binaryfox <binary@foxontheinter.net>
2018-02-17 08:52:44 -0800

    Fix password reset form configuration
    The arguments for PasswordResetView are now passed to the as_view()
    function instead of as kwargs to the path() function.

--- tinyreg/urls.py
+++ tinyreg/urls.py
@@ -11,11 +11,10 @@ urlpatterns = [
          name="password_change"),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(),
          name="password_change_done"),
-    path('password_reset/', auth_views.PasswordResetView.as_view(),
-         {
-             'from_email': settings.ARTSHOW_EMAIL_SENDER,
-             'password_reset_form': CaptchaPasswordResetForm,
-         },
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+             from_email=settings.ARTSHOW_EMAIL_SENDER,
+             form_class=CaptchaPasswordResetForm,
+         ),
          name="password_reset"),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
          name="password_reset_done"),
