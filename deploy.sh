6d9794cc0d774b9c592823dca72359c88cb2294a
binaryfox <binary@foxontheinter.net>
2018-08-12 15:22:09 -0700

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+  echo "First parameter must be the supervisord job name."
+  exit 1
+fi
 
 # Fetch the latest from git
 git pull
+
+# Sync installed packages to Pipfile.lock.
+pipenv install --system
 
 # Apply the latest database migrations
 ./manage.py migrate
@@ -10,4 +18,4 @@ git pull
 ./manage.py collectstatic --noinput
 
 # Trigger a reload of the application
-touch artshowjockey/wsgi.py
+sudo supervisorctl restart "$1"
