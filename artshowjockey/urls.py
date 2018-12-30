c75ab693a8c87d909ea6a71f7dec145caa05948a
binaryfox <binary@foxontheinter.net>
2018-12-30 15:19:15 -0800

    Disable SES bounce notification endpoint
    This removes the m2crypto dependency which has been a general pain.

--- artshowjockey/urls.py
+++ artshowjockey/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.decorators.csrf import csrf_exempt
 from ajax_select import urls as ajax_select_urls
-from django_ses.views import handle_bounce
 
 import artshow.views
 
@@ -15,6 +13,5 @@ urlpatterns = [
     url(r'^manage/', include('artshow.manage_urls')),
     url(r'^peeps/', include('peeps.urls')),
     url(r'^accounts/', include('tinyreg.urls')),
-    url(r'^ses/bounce/$', csrf_exempt(handle_bounce)),
     url(r'^$', artshow.views.home, name="home"),
 ]
