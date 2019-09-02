e7f19a8e5857e796b418d1b27e927dd003191ac8
binaryfox <binary@foxontheinter.net>
2019-09-01 17:48:29 -0700

    Remove singleton configuration of Square API client
    As of version 2.20180712.3 of the Square Connect API the configuration
    class is no longer a singleton and configuration needs to be set on the
    instance being used.

--- artshow/square.py
+++ artshow/square.py
@@ -1,7 +1,6 @@
 import logging
 import uuid
 
-import squareconnect
 from squareconnect.rest import ApiException
 from squareconnect.apis.transactions_api import TransactionsApi
 
@@ -9,12 +8,11 @@ from .conf import settings
 
 logger = logging.getLogger(__name__)
 
-squareconnect.configuration.access_token = settings.ARTSHOW_SQUARE_ACCESS_TOKEN
-location_id = settings.ARTSHOW_SQUARE_LOCATION_ID
-
 
 def charge(payment, nonce):
     api_instance = TransactionsApi()
+    api_instance.api_client.configuration.access_token = \
+        settings.ARTSHOW_SQUARE_ACCESS_TOKEN
     idempotency_key = str(uuid.uuid1())
     body = {
         'idempotency_key': idempotency_key,
@@ -27,6 +25,7 @@ def charge(payment, nonce):
         'note': 'Art Show space reservation for Artist #%d' % payment.artist.artistid,
     }
     try:
+        location_id = settings.ARTSHOW_SQUARE_LOCATION_ID
         api_response = api_instance.charge(location_id, body)
         logger.debug('Square charge successful: %s' % api_response.transaction)
         return api_response.transaction.id
