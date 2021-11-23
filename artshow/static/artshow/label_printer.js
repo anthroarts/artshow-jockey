40b9ecb2028a6a7dd6a10900587015978de58970
binaryfox <binary@foxontheinter.net>
2021-11-22 17:33:24 -0800

    Add label printer integration to artist check-in

---
+++ artshow/static/artshow/label_printer.js
@@ -0,0 +1,64 @@
+const supportedPrinters = [
+    { vendorId: 0x0A5F, productId: 0x0015 },  // Zebra LP-2824
+];
+
+function isSupported(device) {
+    for (const printer of supportedPrinters) {
+        if (device.vendorId == printer.vendorId &&
+            device.productId == printer.productId) {
+            return true;
+        }
+    }
+    return false;
+}
+
+let labelPrinterStatus;
+let selectedDevice = null;
+
+function setUpLabelPrinter(statusElementId) {
+    labelPrinterStatus = document.getElementById(statusElementId);
+
+    navigator.usb.getDevices().then((devices) => {
+        for (const device of devices) {
+            if (isSupported(device)) {
+                selectedDevice = device;
+                labelPrinterStatus.textContent = 'Label printer ready.';
+            }
+        }
+    });
+
+    navigator.usb.addEventListener('connect', (e) => {
+        if (selectedDevice == null && isSupported(e.device)) {
+            selectedDevice = e.device;
+            labelPrinterStatus.textContent = 'Label printer ready.';
+        }
+    });
+
+    navigator.usb.addEventListener('disconnect', (e) => {
+        if (selectedDevice == e.device) {
+            selectedDevice = null;
+            labelPrinterStatus.textContent = 'Label printer disconnected.';
+        }
+    });
+}
+
+async function printLabels(data) {
+    if (selectedDevice === null) {
+        try {
+            selectedDevice = await navigator.usb.requestDevice({ filters: supportedPrinters });
+        } catch (e) {
+            labelPrinterStatus.textContent = 'No printer selected.';
+            return;
+        }
+    }
+
+    await selectedDevice.open();
+
+    try {
+        await selectedDevice.selectConfiguration(1);
+        await selectedDevice.claimInterface(0);
+        await selectedDevice.transferOut(6, new TextEncoder().encode(data));
+    } finally {
+        await selectedDevice.close();
+    }
+}
