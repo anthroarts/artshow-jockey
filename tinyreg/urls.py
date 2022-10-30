226170f218a722707ae1dd614e4094e9b916fe6c
binaryfox <binary@foxontheinter.net>
2022-10-30 12:20:50 -0700

    Add trailing slash to tinyreg URLs

--- tinyreg/urls.py
+++ tinyreg/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', auth_views.LoginView.as_view(), name="login"),
-    path('oauth_redirect', views.oauth_redirect, name='oauth-redirect'),
-    path('oauth_complete', views.oauth_complete, name='oauth-complete'),
-    path('logout', auth_views.LogoutView.as_view(), name="logout"),
+    path('login/', auth_views.LoginView.as_view(), name="login"),
+    path('oauth_redirect/', views.oauth_redirect, name='oauth-redirect'),
+    path('oauth_complete/', views.oauth_complete, name='oauth-complete'),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
 ]
