551c0710b74958949fd7d038da4e14638e9a9ed3
binaryfox <binary@foxontheinter.net>
2022-11-06 09:38:58 -0800

    Remove duplicate logger configuration
    Django already sends ERROR logs to AdminEmailHandler. Specifying it here
    with propagate: True means duplicate messages were being sent.

--- artshowjockey/settings.py
+++ artshowjockey/settings.py
@@ -204,30 +204,15 @@ if TEST_OAUTH_PROVIDER:
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        },
-    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
         },
     },
     'loggers': {
         'artshow.square': {
             'handlers': ['console'],
             'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
             'propagate': True,
         },
         'paypal': {
